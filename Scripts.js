c9fdd8393f9e7b64b6e8423b0cc7e6ab0b1e191e
Midalax <miloszmidalaxmax@interia.pl>
2023-11-23 02:13:13 +0100

    Update Scripts.js

--- Scripts.js
+++ Scripts.js
@@ -6,7 +6,8 @@ var Option_state = [false, false, false, false];
 
 var Option_A_price = 100,
     Option_B_price = 200,
-    Option_C_price = 500;
+    Option_C_price = 500,
+    Option_D_price = 700;
 
 var Supplement_A_price = 10,
     Supplement_B_price = 20,
@@ -15,7 +16,7 @@ var Supplement_A_price = 10,
 
 var price_sum = 0;
 var price_now = 0;
-var Selected_option = 0;
+var Option_price_buffor = 0;
 
 
 
@@ -51,7 +52,7 @@ function Supplement_C() {
 
 
 
-function Deselect() {
+function Deselect(Except, price_except) {
     document.getElementById("Deposit_1").style.outlineColor = "#5207ad";
     document.getElementById("Deposit_2").style.outlineColor = "#5207ad";
     document.getElementById("Deposit_3").style.outlineColor = "#5207ad";
@@ -60,27 +61,26 @@ function Deselect() {
     for (let i = 0; i <= 3; i++)
     {
         Option_state[i]=false;
+        Except=true;
     }
+    console.log(Option_price_buffor);
+
 }
 
 function Select(Option_to_select, OptionPrice)
 {
     Option_to_select.style.outlineColor = "#07ad15";
     console.log(Option_state);
-    if (Option_to_select == true) {
-
-        price(OptionPrice);     
-    } else {
-
-        price(OptionPrice * (-1));
-    }
+    Option_price_buffor=OptionPrice;
+    price(Option_price_buffor);
     //test
 }
 
 function Option_A() {
 
-    Deselect();
+    
     var Option_A = document.getElementById("Deposit_1");
+    Deselect(Option_state[0]);
     Option_state[0] = !Option_state[0];
     Select(Option_A, Option_A_price);
     
@@ -92,7 +92,7 @@ function Option_B() {
     Deselect();
     var Option_B = document.getElementById("Deposit_2");
     Option_state[1] = !Option_state[1];
-    Select(Option_B);
+    Select(Option_B,Option_B_price);
 
 }
 
@@ -100,7 +100,7 @@ function Option_C() {
     Deselect();
     var Option_C = document.getElementById("Deposit_3");
     Option_state[2] = !Option_state[2];
-    Select(Option_C);
+    Select(Option_C,Option_C_price);
 
 }
 
@@ -108,7 +108,7 @@ function Option_D() {
     Deselect();
     var Option_D = document.getElementById("Deposit_4");
     Option_state[3] = !Option_state[3];
-    Select(Option_D);
+    Select(Option_D,Option_D_price);
    
 
 }
